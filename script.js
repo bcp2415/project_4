74bd072900fabc64b6ab4038a42cf89e5e8ba479
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-10 19:42:01 -0500

    Added h1 heading, btn to clear text, functions to count vowels, clear text, display vowel count, eventListener for clear txt btn.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Set variables
 var textEntry = document.querySelector("#textEntry");
 var countVowelsBtn = document.querySelector("#countVowelsBtn");
+var resultDisplay = document.querySelector("#resultDisplay");
+var clearTxtBtn = document.querySelector("#clearTxtBtn");
 
 // focus in textarea
 textEntry.focus();
@@ -9,7 +11,22 @@ function countVowels() {
     // to be filled in
 }
 
+function displayVowels() {
+    // to be filled in...
+}
+
+function clearText() {
+    textEntry.value = "";
+    textEntry.focus();
+}
+
 // event listener on Count the Vowels! button:
 countVowelsBtn.addEventListener("click", function() {
     countVowels();
+    displayVowels();
+});
+
+// event listener on clear txt button:
+clearTxtBtn.addEventListener("click", function() {
+    clearText();
 });
