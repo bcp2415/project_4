3cb7c8af0eaf6cb077d7c2ebdb9b7d9355da20aa
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-10 19:27:47 -0500

    Initial commit.  Bare bones of html, css, and beginnings of js.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Set variables
+var textEntry = document.querySelector("#textEntry");
+var countVowelsBtn = document.querySelector("#countVowelsBtn");
+
+// focus in textarea
+textEntry.focus();
+
+function countVowels() {
+    // to be filled in
+}
+
+// event listener on Count the Vowels! button:
+countVowelsBtn.addEventListener("click", function() {
+    countVowels();
+});
